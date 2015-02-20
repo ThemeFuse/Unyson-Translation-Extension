4a0be4084bb39c679fefcef9c45fb6e4b5bcbd92
themefuse <hello@themefuse.com>
2015-02-20 22:16:53 +0200

    fixed manifest description and title

--- manifest.php
+++ manifest.php
@@ -4,8 +4,8 @@
 
 $manifest = array();
 
-$manifest['name']        = __( 'Translation', 'fw' );
-$manifest['description'] = __( 'This extension translate your blog', 'fw' );
+$manifest['name']        = __( 'Translations', 'fw' );
+$manifest['description'] = __( 'This extension lets you translate your website in any language or even add multiple languages for your users to change at their will from the front-end.', 'fw' );
 
 $manifest['version'] = '1.0.0';
 
