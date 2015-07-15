85f9cbee63ed8bf6a166f3441b1f1067127e27cb
Alexei Gogu <llemurya@gmail.com>
2015-07-15 08:52:20 +0300

    changed manifest version

--- manifest.php
+++ manifest.php
@@ -7,7 +7,7 @@ $manifest = array();
 $manifest['name']        = __( 'Translations', 'fw' );
 $manifest['description'] = __( 'This extension lets you translate your website in any language or even add multiple languages for your users to change at their will from the front-end.', 'fw' );
 
-$manifest['version'] = '1.0.3';
+$manifest['version'] = '1.0.4';
 
 $manifest['display'] = true;
 $manifest['standalone'] = true;
