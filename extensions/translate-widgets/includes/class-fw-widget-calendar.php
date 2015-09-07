418719805739c00f940e3edc95b8b2da13a052a4
moldcraft <moldcraft@email.com>
2015-09-07 22:42:33 +0300

    fixes for https://github.com/ThemeFuse/Unyson/issues/443

--- extensions/translate-widgets/includes/class-fw-widget-calendar.php
+++ extensions/translate-widgets/includes/class-fw-widget-calendar.php
@@ -46,9 +46,9 @@ class FW_Widget_Calendar extends WP_Widget {
 		$instance = wp_parse_args( (array) $instance, array( 'title' => '' ) );
 		$title    = strip_tags( $instance['title'] );
 		?>
-		<p><label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' ); ?></label>
-			<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>"
-			       name="<?php echo $this->get_field_name( 'title' ); ?>" type="text"
+		<p><label for="<?php echo esc_attr($this->get_field_id( 'title' )); ?>"><?php _e( 'Title:' ); ?></label>
+			<input class="widefat" id="<?php echo esc_attr($this->get_field_id( 'title' )); ?>"
+			       name="<?php echo esc_attr($this->get_field_name( 'title' )); ?>" type="text"
 			       value="<?php echo esc_attr( $title ); ?>"/></p>
 	<?php
 	}
